ef6d3ca88ee082cc37131099eca55f47cd0d086c
Ramisa <anjumramisa930@gmail.com>
2021-06-15 01:25:43 +0600

    4-6

---
+++ LAB_TASK_03_PHP_INTRO/largesr_number.php
@@ -0,0 +1,10 @@
+<?php
+$x=15;
+$y=13;
+$z=11;
+if($x>$y && $x>$z)
+{echo "largest number of x is[$x]";}
+elseif($y>$x && $y>$z)
+{echo "largest number of y is[$y]";}
+else{echo "largest number of z is[$z]";}
+?>
---
+++ LAB_TASK_03_PHP_INTRO/searcharray.php
@@ -0,0 +1,7 @@
+<?php
+$a=array("a"=>"10","b"=>"20","c"=>"30");
+for($x=0;$x<=30;$x++){
+	if($x/2==10)
+		echo("The searched number is [$x]"." ");
+}
+?>
