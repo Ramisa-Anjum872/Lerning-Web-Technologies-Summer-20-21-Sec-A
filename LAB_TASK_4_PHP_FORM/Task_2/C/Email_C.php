4530daaf81e22bd8554dbe61868d325eeb138f2c
Ramisa <anjumramisa930@gmail.com>
2021-06-15 19:40:30 +0600

    task_2_email_c

---
+++ LAB_TASK_4_PHP_FORM/Task_2/C/Email_C.php
@@ -0,0 +1,28 @@
+<?php
+
+	$email = "";
+
+	if(isset($_REQUEST['submit'])){
+
+		$email = $_REQUEST['email'];
+		if($email == ""){
+			echo "invalid email!";
+		}
+	}else{
+		echo 'invalid request';
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Data</title>
+</head>
+<body>
+	<form method="post" >
+
+		<h4>Email</h4><input type="email" name="email" value=""/><br>
+                <input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
